62a28d36d75bcb398842bd94bdb29ef880f6cc42
andrewreed <andrewreed2017@icloud.com>
2018-03-02 17:24:21 -0500

    Added a get next player command for the player lobby

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -3,16 +3,23 @@ package com.webcheckers.appl;
 import com.webcheckers.model.Player;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class PlayerLobby {
 
-    private static LinkedList allPlayers;
+    private static Queue<Player> allPlayers;
 
     public static void init(){
-        allPlayers = new LinkedList<Player>();
+        allPlayers = new LinkedList<>();
     }
 
     public static void addPlayer(Player player){
-        allPlayers.add(player);
+        boolean add = allPlayers.add(player);
+        if (add)
+           System.out.println("SERVER:" + player.getName() + " has signed in.");
+    }
+
+    public static Player getNextPlayer(){
+        return allPlayers.remove();
     }
 }
