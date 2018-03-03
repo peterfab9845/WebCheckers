0434f7df7074711ff566d67b39fba27110ef45ab
pnf9945 <pnf9945@rit.edu>
2018-03-03 02:03:39 -0500

    Added model board generation

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -1,12 +1,26 @@
 package com.webcheckers.model;
 
+import com.webcheckers.gameview.PieceColor;
+
 /**
  * Created by Curtis Veronesi on 3/2/2018.
  */
 public class Board {
 
+    private PieceColor[][] board = new PieceColor[8][8];
 
     public Board() {
-
+        //white at the top
+        for (int col = 0; col < 8; col++) {
+            if (col % 2 == 1) {
+                board[0][col] = PieceColor.WHITE;
+                board[2][col] = PieceColor.WHITE;
+                board[6][col] = PieceColor.RED;
+            } else {
+                board[1][col] = PieceColor.WHITE;
+                board[5][col] = PieceColor.RED;
+                board[7][col] = PieceColor.RED;
+            }
+        }
     }
 }
