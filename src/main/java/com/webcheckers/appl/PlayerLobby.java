1a7de96b5349c8a27ffb095a9a2dc9f58772074b
pnf9945 <pnf9945@rit.edu>
2018-03-03 01:12:37 -0500

    Removed redundant allPlayers list

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,73 +1,68 @@
 package com.webcheckers.appl;
 
 import com.webcheckers.model.Player;
+import java.util.Iterator;
 import spark.Session;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class PlayerLobby {
 
-    private static Queue<Player> allPlayers;
-    private static HashMap<String, Player> allPlayersHashed;
+    private static HashMap<String, Player> allPlayers;
 
     public static void init() {
-        allPlayers = new LinkedList<>();
-        allPlayersHashed = new HashMap<>();
+        allPlayers = new HashMap<>();
     }
 
     public static boolean addPlayer(Player player, Session session) {
-        if (allPlayersHashed.containsValue(player))
+        if (allPlayers.containsValue(player)) {
             return false;
+        }
 
-        allPlayers.add(player);
-        allPlayersHashed.put(session.id(), player);
-
+        allPlayers.put(session.id(), player);
         return true;
     }
 
     public static Player getNextPlayer() {
-        return allPlayers.remove();
+        Iterator<String> sessionIterator = allPlayers.keySet().iterator();
+        return allPlayers.remove(sessionIterator.next());
     }
 
-    public static Player getPlayer(Session session){
-        return allPlayersHashed.get(session.id());
+    public static Player getPlayer(Session session) {
+        return allPlayers.get(session.id());
     }
 
-    public static boolean sessionExists(Session session){
-        return allPlayersHashed.containsKey(session.id());
+    public static boolean sessionExists(Session session) {
+        return allPlayers.containsKey(session.id());
     }
 
-    public static int getPlayerCount(){
-        return allPlayersHashed.size();
+    public static int getPlayerCount() {
+        return allPlayers.size();
     }
-    
+
     public static List<String> getPlayerList() {
         List<String> playerNames = new ArrayList<>();
-        for (Player p : allPlayersHashed.values()) {
+        for (Player p : allPlayers.values()) {
             playerNames.add(p.getName());
         }
         return playerNames;
     }
-    
+
     public static List<String> getPlayerListExcept(String name) {
         List<String> playerNames = getPlayerList();
-        if (playerNames.contains(name)) {
-            playerNames.remove(name);
-        }
+        playerNames.remove(name);
         return playerNames;
     }
-    
+
     public static boolean playerExists(String playerName) {
         List<String> playerList = getPlayerList();
         return playerList.contains(playerName);
     }
-    
+
     public static Player getPlayerByName(String playerName) {
-        for (Player p : allPlayersHashed.values()) {
+        for (Player p : allPlayers.values()) {
             if (p.getName().equals(playerName)) {
                 return p;
             }
