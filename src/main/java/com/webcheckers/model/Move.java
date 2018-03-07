6c77bc3e6439d30a9d9c60a3cb2367b40e7908af
Adam Heeter <axh6277@rit.edu>
2018-03-06 19:59:15 -0500

    Created Move and Position classes

---
+++ src/main/java/com/webcheckers/model/Move.java
@@ -0,0 +1,25 @@
+package com.webcheckers.model;
+
+/**
+ * A move from the player.
+ * 
+ * @author Adam Heeter
+ */
+public class Move {
+    private Position start;
+    
+    private Position end;
+    
+    public Move(Position start, Position end) {
+        this.start = start;
+        this.end = end;
+    }
+    
+    public Position getStart() {
+        return start;
+    }
+    
+    public Position getEnd() {
+        return end;
+    }
+}
---
+++ src/main/java/com/webcheckers/model/Position.java
@@ -0,0 +1,25 @@
+package com.webcheckers.model;
+
+/**
+ * A row, cell position.
+ * 
+ * @author Adam Heeter
+ */
+public class Position {
+    private int row;
+    
+    private int cell;
+    
+    public Position(int row, int cell) {
+        this.row = row;
+        this.cell = cell;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getCell() {
+        return cell;
+    }
+}
