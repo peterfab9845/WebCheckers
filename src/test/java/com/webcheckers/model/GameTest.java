39df58fbd663638beaffe935125c51b3d70ce308
Curtis Veronesi <cpv9844@rit.edu>
2018-03-20 17:17:00 -0400

    Worked on GameTest

--- src/test/java/com/webcheckers/model/GameTest.java
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -1,19 +1,45 @@
 package com.webcheckers.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Curtis Veronesi on 3/19/2018.
  */
 public class GameTest {
+
+    private Player red;
+    private Player white;
+    private Board board;
+    private PieceColor whiteColor;
+    private PieceColor redColor;
+
+    private Game game;
+
+    @BeforeEach
+    public void setup(){
+        red = mock(Player.class);
+        white = mock(Player.class);
+        board = mock(Board.class);
+        whiteColor = PieceColor.WHITE;
+        redColor = PieceColor.RED;
+        when(new Board()).thenReturn(board);
+
+        game = new Game(red, white);
+    }
+
     @Test
     void getRedPlayer() {
-
+        assertTrue(game.getRedPlayer() == red);
     }
 
     @Test
     void getWhitePlayer() {
-
+        assertTrue(game.getWhitePlayer() == white);
     }
 
     @Test
