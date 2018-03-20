ae3e105de30189583447d77855dc857a466376b7
andrewreed <andrewreed2017@icloud.com>
2018-03-20 13:44:26 -0400

    Cleaned GetSigninRouteTest to look cleaner

--- src/test/java/com/webcheckers/ui/GetSigninRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetSigninRouteTest.java
@@ -1,17 +1,16 @@
 package com.webcheckers.ui;
 
-import com.webcheckers.appl.Message;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static spark.Spark.get;
@@ -19,16 +18,29 @@ import static spark.Spark.halt;
 
 class GetSigninRouteTest {
 
-    private static final String USER_NAME1 = "Andrew";
-    private static final String USER_NAME2 = "Peter";
+    private static final String VALID_USERNAME = "MyUsername";
     private static final String SESSION_ID = "sessionId";
 
     private TemplateEngine engine;
     private Request request;
     private Response response;
-    private Player player1;
-    private Player player2;
-    private Message message;
+    private Player player;
+    private TemplateEngineTester testHelper;
+
+    @BeforeEach
+    void setup(){
+        request = mock(Request.class);
+        Session session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        when(session.id()).thenReturn(SESSION_ID);
+        engine = mock(TemplateEngine.class);
+        response = mock(Response.class);
+        player = mock(Player.class);
+        when(player.getName()).thenReturn(VALID_USERNAME);
+        PlayerLobby.init();
+        testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    }
 
     @Test
     void constructor(){
@@ -37,47 +49,33 @@ class GetSigninRouteTest {
     }
 
     @Test
-    void handleWithoutTakenUsername() {
-        request = mock(Request.class);
-        Session session = mock(Session.class);
-        when(request.session()).thenReturn(session);
-        when(session.id()).thenReturn(SESSION_ID);
-        engine = mock(TemplateEngine.class);
-        response = mock(Response.class);
-        player1 = mock(Player.class);
-        when(player1.getName()).thenReturn(USER_NAME1);
-        PlayerLobby.init();
-        Map<String, Object> vm = new HashMap<>();
-        vm.put("title", "Sign-in");
-
-        GetSigninRoute getSigninRoute = new GetSigninRoute(engine);
-
-        Object expected = engine.render(new ModelAndView(vm, "signin.ftl"));
-        Object actual = getSigninRoute.handle(request, response);
-
-        assertEquals(actual, expected);
+    void constructor_nullEngine() {
+        engine = null;
+        assertThrows(NullPointerException.class, () -> {
+            final GetHomeRoute getHomeRoute = new GetHomeRoute(engine);
+        }, "GetHomeRoute allowed null template engine.");
     }
 
     @Test
-    void handleWithTakenUsername(){
-        request = mock(Request.class);
-        Session session = mock(Session.class);
-        when(session.id()).thenReturn(SESSION_ID);
-        engine = mock(TemplateEngine.class);
-        response = mock(Response.class);
-        player1 = mock(Player.class);
-        when(player1.getName()).thenReturn(USER_NAME1);
-
-        PlayerLobby.init();
-        PlayerLobby.addPlayer(player1, session);
-        when(request.session()).thenReturn(session);
+    void handleWithoutTakenSession() {
 
         GetSigninRoute getSigninRoute = new GetSigninRoute(engine);
-        try {
-            getSigninRoute.handle(request, response);
-        }
-        catch (HaltException e){
-            e.printStackTrace();
-        }
+        getSigninRoute.handle(request, response);
+
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        testHelper.assertViewModelAttribute("title", "Sign-in");
+
+    }
+
+    @Test
+    void handleWithTakenSession(){
+
+
+        PlayerLobby.addPlayer(player, request.session());
+        GetSigninRoute getSigninRoute = new GetSigninRoute(engine);
+        try { getSigninRoute.handle(request, response); }
+        catch (HaltException ignored){}
+
     }
 }
