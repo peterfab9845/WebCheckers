43159c85527d4a23279e0e0f79fd01116915977d
Curtis <cpv9844@rit.edu>
2018-03-20 23:31:59 -0400

    Fixed GameTest

--- src/test/java/com/webcheckers/model/GameTest.java
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -1,9 +1,13 @@
 package com.webcheckers.model;
 
 import com.webcheckers.gameview.BoardView;
+import com.webcheckers.gameview.Row;
+import com.webcheckers.gameview.Space;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -18,11 +22,6 @@ public class GameTest {
 
     private Player red;
     private Player white;
-    private Board board;
-    private PieceColor redColor;
-    private PieceColor whiteColor;
-    private BoardView boardViewRed;
-    private BoardView boardViewWhite;
 
     private Game game;
 
@@ -30,14 +29,6 @@ public class GameTest {
     public void setup(){
         red = mock(Player.class);
         white = mock(Player.class);
-        board = mock(Board.class);
-        whiteColor = PieceColor.WHITE;
-        redColor = PieceColor.RED;
-        boardViewRed = mock(BoardView.class);
-        boardViewWhite = mock(BoardView.class);
-        when(new Board()).thenReturn(board);
-        when(board.getBoardView(PieceColor.RED)).thenReturn(boardViewRed);
-        when(board.getBoardView(PieceColor.WHITE)).thenReturn(boardViewWhite);
 
         game = new Game(red, white);
     }
@@ -53,14 +44,52 @@ public class GameTest {
     }
 
     @Test
-    void getBoardView() {
-        assertTrue(game.getBoardView(red) == boardViewRed);
-        assertTrue(game.getBoardView(white) == boardViewWhite);
+    void getBoardViewWhite() {
+        Board testBoard = new Board();
+        BoardView testBoardBoardView = testBoard.getBoardView(PieceColor.WHITE);
+        BoardView gameTestBoardView = game.getBoardView(white);
+        Iterator<Row> testBoardRows = testBoardBoardView.iterator();
+        Iterator<Row> gameBoardRows = gameTestBoardView.iterator();
+        while(testBoardRows.hasNext()){
+            Row testBoardRow = testBoardRows.next();
+            Row gameBoardRow = gameBoardRows.next();
+            Iterator<Space> testBoardSpaces = testBoardRow.iterator();
+            Iterator<Space> gameBoardSpaces = gameBoardRow.iterator();
+            while (testBoardSpaces.hasNext()){
+                Space testBoardSpace = testBoardSpaces.next();
+                Space gameBoardSpace = gameBoardSpaces.next();
+                Piece testBoardPiece = testBoardSpace.getPiece();
+                Piece gameBoardPiece = gameBoardSpace.getPiece();
+                assertTrue(testBoardPiece.getColor() == gameBoardPiece.getColor() && testBoardPiece.getType() == gameBoardPiece.getType());
+            }
+        }
+    }
+
+    @Test
+    void getBoardViewRed(){
+        Board testBoard = new Board();
+        BoardView testBoardBoardView = testBoard.getBoardView(PieceColor.RED);
+        BoardView gameTestBoardView = game.getBoardView(red);
+        Iterator<Row> testBoardRows = testBoardBoardView.iterator();
+        Iterator<Row> gameBoardRows = gameTestBoardView.iterator();
+        while(testBoardRows.hasNext()){
+            Row testBoardRow = testBoardRows.next();
+            Row gameBoardRow = gameBoardRows.next();
+            Iterator<Space> testBoardSpaces = testBoardRow.iterator();
+            Iterator<Space> gameBoardSpaces = gameBoardRow.iterator();
+            while (testBoardSpaces.hasNext()){
+                Space testBoardSpace = testBoardSpaces.next();
+                Space gameBoardSpace = gameBoardSpaces.next();
+                Piece testBoardPiece = testBoardSpace.getPiece();
+                Piece gameBoardPiece = gameBoardSpace.getPiece();
+                assertTrue(testBoardPiece.getColor() == gameBoardPiece.getColor() && testBoardPiece.getType() == gameBoardPiece.getType());
+            }
+        }
     }
 
     @Test
     void getActiveColor() {
-        assertTrue(game.getActiveColor() == redColor);
+        assertTrue(game.getActiveColor() == PieceColor.RED);
     }
 
 }
