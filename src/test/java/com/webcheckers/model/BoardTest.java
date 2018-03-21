3a95785315a971b35d968966f07769ee7057257b
Curtis <cpv9844@rit.edu>
2018-03-20 21:49:16 -0400

    Implemented BoardTest and GameTest

--- src/test/java/com/webcheckers/model/BoardTest.java
+++ src/test/java/com/webcheckers/model/BoardTest.java
@@ -1,7 +1,17 @@
 package com.webcheckers.model;
 
+import com.webcheckers.gameview.BoardView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Any;
+import org.mockito.stubbing.OngoingStubbing;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockingDetails;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Curtis Veronesi on 3/19/2018.
@@ -9,9 +19,56 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("WeakerAccess")
 @Tag("Model-Tier")
 public class BoardTest {
-    @Test
-    void getBoardView() {
+    private Piece whitePiece;
+    private Piece redPiece;
+    private Piece[][] whitePieceBoard = {
+            {null, redPiece, null, redPiece, null, redPiece, null, redPiece},
+            {redPiece, null, redPiece, null, redPiece, null, redPiece, null},
+            {null, redPiece, null, redPiece, null, redPiece, null, redPiece},
+            {null, null, null, null, null, null, null, null},
+            {null, null, null, null, null, null, null, null},
+            {whitePiece, null, whitePiece, null, whitePiece, null, whitePiece, null},
+            {null, whitePiece, null, whitePiece, null, whitePiece, null, whitePiece},
+            {whitePiece, null, whitePiece, null, whitePiece, null, whitePiece, null}
+    };
+    private Piece[][] redPieceBoard = {
+            {null, whitePiece, null, whitePiece, null, whitePiece, null, whitePiece},
+            {whitePiece, null, whitePiece, null, whitePiece, null, whitePiece, null},
+            {null, whitePiece, null, whitePiece, null, whitePiece, null, whitePiece},
+            {null, null, null, null, null, null, null, null},
+            {null, null, null, null, null, null, null, null},
+            {redPiece, null, redPiece, null, redPiece, null, redPiece, null},
+            {null, redPiece, null, redPiece, null, redPiece, null, redPiece},
+            {redPiece, null, redPiece, null, redPiece, null, redPiece, null}
+    };
+    private BoardView whiteBoardView;
+    private BoardView redBoardView;
 
+    private Board board;
+
+    @BeforeEach
+    public void setup(){
+        whitePiece = mock(Piece.class);
+        redPiece = mock(Piece.class);
+        whiteBoardView = mock(BoardView.class);
+        redBoardView = mock(BoardView.class);
+        when(new Piece(PieceType.SINGLE, PieceColor.WHITE)).thenReturn(whitePiece);
+        when(new Piece(PieceType.SINGLE, PieceColor.RED)).thenReturn(redPiece);
+        when(new BoardView(whitePieceBoard)).thenReturn(whiteBoardView);
+        when(new BoardView(redPieceBoard)).thenReturn(redBoardView);
+
+        board = new Board();
+    }
+
+
+    @Test
+    void getBoardViewWhite() {
+        assertTrue(board.getBoardView(PieceColor.WHITE) == whiteBoardView);
+    }
+
+    @Test
+    void getBoardViewRed(){
+        assertTrue(board.getBoardView(PieceColor.RED) == redBoardView);
     }
 
 
--- src/test/java/com/webcheckers/model/GameTest.java
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -1,6 +1,8 @@
 package com.webcheckers.model;
 
+import com.webcheckers.gameview.BoardView;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,13 +12,17 @@ import static org.mockito.Mockito.when;
 /**
  * Created by Curtis Veronesi on 3/19/2018.
  */
+@SuppressWarnings("WeakerAccess")
+@Tag("Model-Tier")
 public class GameTest {
 
     private Player red;
     private Player white;
     private Board board;
-    private PieceColor whiteColor;
     private PieceColor redColor;
+    private PieceColor whiteColor;
+    private BoardView boardViewRed;
+    private BoardView boardViewWhite;
 
     private Game game;
 
@@ -27,7 +33,11 @@ public class GameTest {
         board = mock(Board.class);
         whiteColor = PieceColor.WHITE;
         redColor = PieceColor.RED;
+        boardViewRed = mock(BoardView.class);
+        boardViewWhite = mock(BoardView.class);
         when(new Board()).thenReturn(board);
+        when(board.getBoardView(PieceColor.RED)).thenReturn(boardViewRed);
+        when(board.getBoardView(PieceColor.WHITE)).thenReturn(boardViewWhite);
 
         game = new Game(red, white);
     }
@@ -44,12 +54,13 @@ public class GameTest {
 
     @Test
     void getBoardView() {
-
+        assertTrue(game.getBoardView(red) == boardViewRed);
+        assertTrue(game.getBoardView(white) == boardViewWhite);
     }
 
     @Test
     void getActiveColor() {
-
+        assertTrue(game.getActiveColor() == redColor);
     }
 
 }
