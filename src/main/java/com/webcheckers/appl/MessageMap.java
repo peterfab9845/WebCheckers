99326bde0fbab5649c5cbf8a052fcbc0a16031a1
andrew <acr7950@g.rit.edu>
2018-03-31 01:54:12 -0400

    Message Classes Added

---
+++ src/main/java/com/webcheckers/appl/MessageMap.java
@@ -0,0 +1,50 @@
+package com.webcheckers.appl;
+
+import spark.Session;
+
+import java.util.HashMap;
+
+/**
+ * Manages a HashMap of sessions and message objects.
+ * This way a message can be set on one page and displayed on another.
+ * 
+ * @author Adam Heeter
+ *
+ */
+public class MessageMap {
+
+    /**
+     * Map to relate messages to the sessions which need to see them.
+     */
+    private static HashMap<String, Message> messages;
+
+    /**
+     * Initialize the map.
+     */
+    public static void init() {
+        messages = new HashMap<>();
+    }
+
+    /**
+     * Gets the message for the given session and remove it from the list.
+     * @param session the session which requests a message
+     * @return the message for that session, if any; otherwise null
+     */
+    public static Message getMessage(Session session) {
+        if (messages.containsKey(session.id())) {
+            // Remove the message from the list so it only gets displayed once
+            return messages.remove(session.id());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Add a message to the list so that it can be retrieved later.
+     * @param session the session which the message is for
+     * @param newMessage the message for that session
+     */
+    public static void setMessage(Session session, Message newMessage) {
+        messages.put(session.id(), newMessage);
+    }
+}
---
+++ src/main/java/com/webcheckers/model/States/MessageType.java
@@ -0,0 +1,9 @@
+package com.webcheckers.model.States;
+
+/**
+ * The two types of messages.
+ * Used to determine how the message text should be presented.
+ */
+public enum MessageType {
+    info, error
+}
