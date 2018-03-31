abae1376ad507f97ed54f6adaf46c906ad2964b0
andrew <acr7950@g.rit.edu>
2018-03-31 01:54:38 -0400

    hashCode function added to player

--- src/main/java/com/webcheckers/model/Entities/Player.java
+++ src/main/java/com/webcheckers/model/Entities/Player.java
@@ -1,16 +1,28 @@
 package com.webcheckers.model.Entities;
 
 import com.webcheckers.model.States.PlayerStates;
+import spark.Session;
 
 public class Player extends Entity {
 
   private PlayerStates currentState;
+  private Session session;
 
-  public Player(String userName){
+  public Player(String userName, Session session){
     super(userName);
+    this.session = session;
   }
 
   public void sendToLobby(){
     this.currentState = PlayerStates.INLOBBY;
+  }
+
+  public boolean isInLobby(){
+    return currentState == PlayerStates.INLOBBY;
+  }
+
+  @Override
+  public int hashCode(){
+    return Integer.parseInt(session.id());
   }
 }
