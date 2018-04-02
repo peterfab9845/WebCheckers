1098cbf8750e7b6e029b43df619870114ac6d5ce
andrewreed <andrewreed2017@icloud.com>
2018-04-02 16:03:45 -0400

    PostBackupMoveRouteTest created

---
+++ src/test/java/com/webcheckers/ui/Movement/PostBackupMoveRouteTest.java
@@ -0,0 +1,23 @@
+package com.webcheckers.ui.Movement;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.omg.CORBA.Request;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class PostBackupMoveRouteTest {
+
+    private PostBackupMoveRoute CuT;
+
+    @BeforeEach
+    void setUp() {
+        Request request = mock(Request.class);
+        CuT = new PostBackupMoveRoute();
+    }
+
+    @Test
+    void handle() {
+    }
+}
