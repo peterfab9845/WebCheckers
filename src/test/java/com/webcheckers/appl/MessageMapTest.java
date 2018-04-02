406d9fae57afa6d59792b20b2b35eab46e629efa
andrewreed <andrewreed2017@icloud.com>
2018-04-02 03:52:44 -0400

    MessageMap Test Created

---
+++ src/test/java/com/webcheckers/appl/MessageMapTest.java
@@ -0,0 +1,39 @@
+package com.webcheckers.appl;
+
+import com.webcheckers.model.States.MessageType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.Session;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+
+class MessageMapTest {
+
+    private final String MESSAGE = "Message";
+    private Message message;
+    private Session session;
+
+    @BeforeEach
+    void init() {
+        MessageMap map = new MessageMap();
+        MessageMap.init();
+        session = mock(Session.class);
+        message = new Message(MESSAGE, MessageType.info);
+    }
+
+    @Test
+    void getMessage() {
+        MessageMap.setMessage(session, message);
+        Message actual = MessageMap.getMessage(session);
+        assertEquals(message, actual);
+    }
+
+    @Test
+    void getNullMessage() {
+        Message actual = MessageMap.getMessage(session);
+        assertEquals(null, actual);
+    }
+
+}
