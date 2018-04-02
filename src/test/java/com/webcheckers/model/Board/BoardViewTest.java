300c9d6eb0227c1c19fcc979430f70f8bc1abf5f
andrew <acr7950@g.rit.edu>
2018-04-02 05:01:32 -0400

    BoardViewTest class written

---
+++ src/test/java/com/webcheckers/model/Board/BoardViewTest.java
@@ -0,0 +1,33 @@
+package com.webcheckers.model.Board;
+
+import com.webcheckers.model.States.PieceColor;
+import com.webcheckers.model.States.PieceType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardViewTest {
+
+  private BoardView CuT;
+  private Space[][] board;
+
+  @BeforeEach
+  void setUp() {
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        board[row][col] = new Space(col, null);
+      }
+    };
+    CuT = new BoardView(board);
+  }
+
+  @Test
+  void iterator() {
+    Iterator iterator = CuT.iterator();
+    while ( iterator.hasNext() )
+      iterator.next();
+  }
+}
