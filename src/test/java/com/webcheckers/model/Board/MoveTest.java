2c2880484493b9acefa2d2bb7794dd0cc26eb695
andrew <acr7950@g.rit.edu>
2018-04-02 05:01:43 -0400

    MoveTest class written

---
+++ src/test/java/com/webcheckers/model/Board/MoveTest.java
@@ -0,0 +1,32 @@
+package com.webcheckers.model.Board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveTest {
+
+  private Move CuT;
+  private Position start;
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void getStart() {
+  }
+
+  @Test
+  void getEnd() {
+  }
+
+  @Test
+  void equals() {
+  }
+
+  @Test
+  void iterator() {
+  }
+}
