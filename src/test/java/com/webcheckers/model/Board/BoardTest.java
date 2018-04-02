04c00b0850a7688af7787ee43815bd7105677f08
andrewreed <andrewreed2017@icloud.com>
2018-04-02 15:30:48 -0400

    BoardTest, PieceTest, PlayerTest

--- src/test/java/com/webcheckers/model/Board/BoardTest.java
+++ src/test/java/com/webcheckers/model/Board/BoardTest.java
@@ -10,33 +10,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
-  private Board CuT;
+    private Board CuT;
 
-  @BeforeEach
-  void setup(){
-    CuT = new Board();
-  }
+    @BeforeEach
+    void setup(){
+        CuT = new Board();
+    }
 
-  @Test
-  void getBoardView() {
-    CuT.getBoardView(PieceColor.RED);
-    CuT.getBoardView(PieceColor.WHITE);
-  }
+    @Test
+    void getBoardView() {
+        CuT.getBoardView(PieceColor.RED);
+        CuT.getBoardView(PieceColor.WHITE);
+    }
 
-  @Test
-  void valueAt() {
-    Position position = new Position(0, 1);
-    Piece actual = CuT.valueAt(position);
-    Piece expected = new Piece(PieceType.SINGLE, PieceColor.WHITE);
-    assertEquals(expected, actual);
-  }
+    @Test
+    void valueAt() {
+        Position position = new Position(0, 1);
+        Piece actual = CuT.valueAt(position);
+        Piece expected = new Piece(PieceType.SINGLE, PieceColor.WHITE);
+        assertEquals(expected, actual);
+    }
 
-  @Test
-  void makeMove() {
-    Position start = new Position(0, 1);
-    Piece expected = CuT.valueAt(start);
-    Position end = new Position(0, 1);
-    CuT.makeMove(new Move(start, end));
-    Piece actual = CuT.valueAt(end);
-  }
+    @Test
+    void makeMove() {
+        Position start = new Position(0, 1);
+        Piece expected = CuT.valueAt(start);
+        Position end = new Position(0, 1);
+        CuT.makeMove(new Move(start, end));
+        Piece actual = CuT.valueAt(end);
+    }
 }
---
+++ src/test/java/com/webcheckers/model/Entities/PlayerTest.java
@@ -0,0 +1,52 @@
+package com.webcheckers.model.Entities;
+
+import com.webcheckers.model.GameSaves.GameLog;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.Session;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class PlayerTest {
+
+    private Player CuT;
+    private Session session;
+    private GameLog gameLog;
+    private final String USERNAME = "User";
+
+    @BeforeEach
+    void setUp() {
+        session = mock(Session.class);
+        gameLog = mock(GameLog.class);
+        CuT = new Player(USERNAME, session);
+    }
+
+    @Test
+    void getSession() {
+        Session sessionTest = CuT.getSession();
+        assertEquals(sessionTest, session);
+    }
+
+    @Test
+    void getGameLogs() {
+        CuT.getGameLogs();
+    }
+
+    @Test
+    void deleteGame() {
+        CuT.saveGame(gameLog);
+        CuT.deleteGame(gameLog.toString());
+        CuT.deleteGame(null);
+    }
+
+    @Test
+    void saveGame() {
+        CuT.saveGame(gameLog);
+    }
+
+    @Test
+    void hashCode_Test() {
+        CuT.hashCode();
+    }
+}
