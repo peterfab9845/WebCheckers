6624b10237a559faa9f3f9503d3a2cc605b49f98
andrewreed <andrewreed2017@icloud.com>
2018-04-02 23:36:00 -0400

    hasValidMove Started isMoveValid now checks for spaces away ( King and single )

--- src/main/java/com/webcheckers/appl/MoveChecker.java
+++ src/main/java/com/webcheckers/appl/MoveChecker.java
@@ -1,13 +1,40 @@
 package com.webcheckers.appl;
 
 import com.webcheckers.model.Board.Move;
+import com.webcheckers.model.Board.Piece;
 import com.webcheckers.model.Board.Position;
+import com.webcheckers.model.Board.Space;
 
 public class MoveChecker {
 
-    public static boolean isMoveValid(Move move){
+    private static final int SINGLE_DISTANCE = 2;
+    private static final int KING_DISTANCE = 3;
+
+
+    public static boolean hasValidMove(Position position, Space[][] board){
+        int startX = position.getCell();
+        int startY = position.getRow();
+        Position placeInQuestion;
+        Move move;
+        for( int row = -1; row < 2; row+=2){
+            for( int col = -1; col < 2; col+=2) {
+                placeInQuestion = new Position(startY + row, startX + col);
+                move = new Move(position, placeInQuestion);
+                if(isMoveValid(move, board))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean isMoveValid(Move move, Space[][] board){
         if( !positionIsBlack(move.getEnd()) )
             return false;
+        if( !inDistanceOf(move, SINGLE_DISTANCE) )
+            return false;
+
+
         return true;
     }
 
@@ -18,7 +45,23 @@ public class MoveChecker {
             return position.getCell() % 2 == 0;
     }
 
-//    public boolean validSpace(Position position){
-//        return board.valueAt(position) == null;
-//    }
+    private static boolean inDistanceOf(Move move, int dist){
+        int x1 = move.getStartingX();
+        int x2 = move.getEndingX();
+        int deltaX = x1 - x2;
+        int y1 = move.getStartingY();
+        int y2 = move.getEndingY();
+        int deltaY = y1 - y2;
+        if( deltaX == 0 )
+            return false;
+        int distance = deltaY/ deltaX;
+        return Math.abs(distance) < dist;
+    }
+
+
+    private static Piece pieceAt(Position position, Space[][] board){
+        int x = position.getCell();
+        int y = position.getRow();
+        return board[y][x].getPiece();
+    }
 }
