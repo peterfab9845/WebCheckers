af50718f6d8093ae4eadedb396f76fc9a0b353d2
pnf9945 <pnf9945@rit.edu>
2018-04-14 15:14:36 -0400

    Created tests for GameManager

--- src/main/java/com/webcheckers/appl/PlayerLobby/GameManager.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby/GameManager.java
@@ -3,45 +3,55 @@ package com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.States.PieceColor;
 import com.webcheckers.model.entities.Game;
 import com.webcheckers.model.entities.Player;
-
 import java.util.HashMap;
 
 public class GameManager {
 
     private HashMap<String, Game> games;
 
-    public GameManager(){
+    public GameManager() {
         games = new HashMap<>();
+    }
+
+
+    /**
+     * Adds a game to the game library
+     */
+    public void addGame(Player player, Game game) {
+        games.put(player.getName(), game);
+    }
+
+    /**
+     * Given a player, returns the game that they are in
+     */
+    public Game getGame(Player player) {
+        return games.get(player.getName());
     }
 
     /**
      * Removes game from list of games, and assigns winners
-     * @param player
      */
-    public void removeGame(Player player){
-        Game game = getGame(player);
+    public void removeGame(Player loser) {
+        Game game = getGame(loser);
         Player redPlayer = game.getRedPlayer();
         Player whitePlayer = game.getWhitePlayer();
 
-        if( player == whitePlayer ){
+        if (loser.equals(redPlayer)) {
+            redPlayer.justLost();
+            whitePlayer.justWon();
+        } else {
             redPlayer.justWon();
             whitePlayer.justLost();
         }
-        else{
-            redPlayer.justLost();
-            whitePlayer.justWon();
-        }
 
-        games.remove(game.getWhitePlayer());
-        games.remove(game.getRedPlayer());
+        games.remove(redPlayer.getName());
+        games.remove(whitePlayer.getName());
     }
 
     /**
-     * puts two players in a game together
-     * @param player
-     * @param challenging
+     * Puts two players in a game together
      */
-    public void challenge(Player player, Player challenging){
+    public void challenge(Player player, Player challenging) {
         player.setInGame();
         challenging.setInGame();
         Game game = new Game(player, challenging);
@@ -51,23 +61,4 @@ public class GameManager {
         addGame(challenging, game);
     }
 
-    /**
-     * Adds a game to the game library
-     * @param player
-     * @param game
-     */
-    public void addGame(Player player, Game game){
-        games.put(player.getName(), game);
-    }
-
-
-
-    /**
-     * Given a player returns the game that they are in
-     * @param player
-     * @return
-     */
-    public Game getGame(Player player){
-        return games.get(player.getName());
-    }
 }
