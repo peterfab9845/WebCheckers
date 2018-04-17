836f7960550f1fa6bba16d267e8b19184bd52b4e
pnf9945 <pnf9945@rit.edu>
2018-04-17 02:01:18 -0400

    Added tests for enums

--- src/test/java/com/webcheckers/model/States/PieceTypeTest.java
+++ src/test/java/com/webcheckers/model/States/PieceTypeTest.java
@@ -1,5 +1,32 @@
 package com.webcheckers.model.States;
 
-public class PieceTypeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for PieceType
+ */
+@Tag("Model-tier")
+class PieceTypeTest {
+
+    /**
+     * Test SINGLE value
+     */
+    @Test
+    void SINGLE_test() {
+        PieceType play = PieceType.SINGLE;
+        assertEquals(play, PieceType.valueOf("SINGLE"));
+    }
+
+    /**
+     * Test KING value
+     */
+    @Test
+    void KING_test() {
+        PieceType play = PieceType.KING;
+        assertEquals(play, PieceType.valueOf("KING"));
+    }
 
 }
--- src/test/java/com/webcheckers/model/States/PlayerStatesTest.java
+++ src/test/java/com/webcheckers/model/States/PlayerStatesTest.java
@@ -1,5 +1,49 @@
 package com.webcheckers.model.States;
 
-public class PlayerStatesTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for PlayerStates
+ */
+@Tag("Model-tier")
+class PlayerStatesTest {
+
+    /**
+     * Test IN_LOBBY value
+     */
+    @Test
+    void IN_LOBBY_test() {
+        PlayerStates play = PlayerStates.IN_LOBBY;
+        assertEquals(play, PlayerStates.valueOf("IN_LOBBY"));
+    }
+
+    /**
+     * Test WON value
+     */
+    @Test
+    void WON_test() {
+        PlayerStates play = PlayerStates.WON;
+        assertEquals(play, PlayerStates.valueOf("WON"));
+    }
+
+    /**
+     * Test LOSS value
+     */
+    @Test
+    void LOSS_test() {
+        PlayerStates play = PlayerStates.LOSS;
+        assertEquals(play, PlayerStates.valueOf("LOSS"));
+    }
+
+    /**
+     * Test IN_GAME value
+     */
+    @Test
+    void IN_GAME_test() {
+        PlayerStates play = PlayerStates.IN_GAME;
+        assertEquals(play, PlayerStates.valueOf("IN_GAME"));
+    }
 }
