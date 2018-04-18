50c77956fb6827e73aaa5e1d69acb1ff4ba1e56a
Curtis Veronesi <cpv9844@rit.edu>
2018-04-18 16:37:44 -0400

    Added medium AI Test

--- src/test/java/com/webcheckers/model/entities/ai/MediumAITest.java
+++ src/test/java/com/webcheckers/model/entities/ai/MediumAITest.java
@@ -1,14 +1,20 @@
 package com.webcheckers.model.entities.ai;
 
+import com.webcheckers.appl.BoardController;
 import com.webcheckers.appl.playerlobby.PlayerLobby;
 import com.webcheckers.model.board.Board;
+import com.webcheckers.model.board.Move;
+import com.webcheckers.model.board.Position;
 import com.webcheckers.model.entities.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.webcheckers.model.entities.ai.AI;
 import com.webcheckers.model.entities.ai.MediumAI;
 
+import java.util.ArrayList;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -25,12 +31,17 @@ public class MediumAITest {
     public void setup(){
         enemy = mock(Player.class);
         lobby = mock(PlayerLobby.class);
-        board = mock(Board.class);
+        board = new Board();
         CuT = new MediumAI(name, enemy, lobby);
     }
 
     @Test
-    public void makeDecisionTest(){
-
+    public void makeJumpDecisionTest(){
+        board.clearSpace(new Position(2,6));
+        Move moveRedToJumpable = new Move(new Position(6,6), new Position(3,5));
+        BoardController.makeMove(board, moveRedToJumpable);
+        when(CuT.getGame(lobby).getBoard()).thenReturn(board);
+        CuT.makeDecision();
+        Move correctMove = new Move(new Position(2,4), new Position(4,6));
     }
 }
