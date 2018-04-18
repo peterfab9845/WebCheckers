f3843ea4ce2fc922351da5ac39d9070a3e63bde5
andrewreed <andrewreed2017@icloud.com>
2018-04-18 01:04:13 -0400

    Currently Hand Merging with spectating branch, there was an error before and i dont want to risk all that work again

--- src/main/resources/public/js/game/modes/spectator/SpectatorModeStartState.js
+++ src/main/resources/public/js/game/modes/spectator/SpectatorModeStartState.js
@@ -1,50 +1,50 @@
 /**
  * This module exports the SpectatorModeStartState class constructor.
- * 
+ *
  * This component is an concrete implementation of a state
  * for the Game view; this state represents the starting state
  * of the Spectator mode.
- * 
+ *
  * This is an exercise for the student.
  */
 define(function(require){
-  'use strict';
-  
-  // imports
-  var SpectatorModeConstants = require('./SpectatorModeConstants');
+    'use strict';
 
-  /**
-   * Constructor function.
-   * 
-   * @param {SpectatorController} controller
-   *    The Spectator mode controller object.
-   */
-  function SpectatorModeStartState(controller) {
-    // private attributes
-    this._controller = controller;
-  };
+    // imports
+    var SpectatorModeConstants = require('./SpectatorModeConstants');
 
-  //
-  // Public (external) methods
-  //
+    /**
+     * Constructor function.
+     *
+     * @param {SpectatorController} controller
+     *    The Spectator mode controller object.
+     */
+    function SpectatorModeStartState(controller) {
+        // private attributes
+        this._controller = controller;
+    };
 
-  /**
-   * Get the symbolic name this state.
-   */
-  SpectatorModeStartState.prototype.getName = function getName() {
-    return SpectatorModeConstants.SPECTATOR_MODE_STARTING;
-  };
-  
-  /**
-   * Method when entering this state.
-   * 
-   * Build and begin the Spectator view mode.
-   */
-  SpectatorModeStartState.prototype.onEntry = function onEntry() {
-    alert('View mode not yet implemented: SPECTATOR');
-  };
-  
-  // export class constructor
-  return SpectatorModeStartState;
-  
+    //
+    // Public (external) methods
+    //
+
+    /**
+     * Get the symbolic name this state.
+     */
+    SpectatorModeStartState.prototype.getName = function getName() {
+        return SpectatorModeConstants.SPECTATOR_MODE_STARTING;
+    };
+
+    /**
+     * Method when entering this state.
+     *
+     * Build and begin the Spectator view mode.
+     */
+    SpectatorModeStartState.prototype.onEntry = function onEntry() {
+        this._controller.enableButton(PlayModeConstants.QUIT_SPEC_BUTTON);
+    };
+
+    // export class constructor
+    return SpectatorModeStartState;
+
 });
