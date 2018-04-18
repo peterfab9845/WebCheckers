bacd63829b7676395c0ae8263cac68202fda34de
andrewreed <andrewreed2017@icloud.com>
2018-04-18 00:16:03 -0400

    screw git for being dumb and removing everything ive done tonight, uncalled for...

--- src/main/java/com/webcheckers/Message.java
+++ src/main/java/com/webcheckers/Message.java
@@ -37,11 +37,6 @@ public class Message {
         return type;
     }
 
-    @Override
-    public String toString() {
-        return text;
-    }
-
     /**
      * Check whether this message is the same, in type and text, as another.
      * @param obj the message with which to check equality
--- src/main/resources/spark/template/freemarker/signin.ftl
+++ src/main/resources/spark/template/freemarker/signin.ftl
@@ -22,7 +22,7 @@
       <h2>Sign in:</h2>
       
       <#if message??>
-      <div class="error">${message.text}</div>
+      <div class="error">${message}</div>
       </#if>
       
       <form action="/signin" method="POST">
